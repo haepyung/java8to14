c73e641f036a818e776c80b802f1aaa6344dfff4
haepyung <haepyung88@gmail.com>
2021-02-11 18:07:58 +0900

    etc: java study-4
    - java 8 study -end

--- src/test/java/me/study/java8to14/Java8to14ApplicationTests.java
+++ src/test/java/me/study/java8to14/Java8to14ApplicationTests.java
@@ -3,11 +3,17 @@ package me.study.java8to14;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 @SpringBootTest
 class Java8to14ApplicationTests {
 
   @Test
   void contextLoads() {
+
+    LinkedList<String> list = new LinkedList<>();
   }
 
 }
